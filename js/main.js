0623e8cca38761ce2d1cb9eede5dcbbc45811097
mikemoore1618 <mikemoore1618@gmail.com>
2018-04-23 18:07:49 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,32 +1,40 @@
-// Create array to store all cards
+// Create an array to store all cards
 var cards = ['queen', 'queen', 'king', 'king'];
 
 // Create array to store cards in play
 var cardsInPlay = [];
 
-// Create variable cardOne to represent first card user flipped
-var cardOne = cards[0];
-// Add card to array of cards in play
-cardsInPlay.push(cardOne);
-// Use console.log() to display card that user flipped
-console.log("User flipped " + cardOne);
-
-// Create variable cardTwo to represent second card user flipped
-var cardTwo = cards[2];
-// Add card to array of cards in play
-cardsInPlay.push(cardTwo);
-// Use console.log() to display card that user flipped
-console.log("User flipped " + cardTwo);
-
-
-// Check to see if two cards have been played
-if (cardsInPlay.length === 2) {
-
-  // Check to see if two cards match and provide feedback to user
+// Create function to check for match
+var checkForMatch = function () {
+  // Check if two cards match and provide feedback to user
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
+    console.log("You found a match!");
   } else {
-    alert("Sorry, try again.");
+    console.log("Sorry, try again.");
 
   }
-}
+};
+
+// Create function to representa  user flipping card
+var flipCard = function (cardId) {
+
+  // Display card user flipped in console.
+  console.log("User flipped " + cards[cardId]);
+
+  // Add card to array of cards in play
+  cardsInPlay.push(cards[cardId]);
+
+  // Check if two cards have been played
+  if (cardsInPlay.length === 2) {
+    // If so, call checkForMatch function
+    checkForMatch();
+  }
+};
+
+
+// For now, we are manually calling the flipCard function
+// to represent a user's play
+// Call the flipCard function, passing in an index as the argument
+flipCard(0);
+// Call the flipCard function, passing in an index as the argument
+flipCard(2);

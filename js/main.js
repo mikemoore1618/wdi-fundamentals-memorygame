c4c0273dba9b0f04ec143203b878ada9b8793546
mikemoore1618 <mikemoore1618@gmail.com>
2018-04-27 18:38:44 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -27,28 +27,25 @@ var cardsInPlay = [];
 
 // Create function to check for match
 var checkForMatch = function () {
-  // Check if two cards match and provide uswe feedback
+  // Check if two cards match and provide feedback to user
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("You found a match!");
+    alert("You found a match!");
   } else {
     alert("Sorry, try again.");
   }
 };
 
 // Create function to represent user flipping card
-var flipCard = function (cardId) {
+var flipCard = function () {
+  // Get data-id of card that was just flipped and cache it
+  cardId = this.getAttribute('data-id');
+  console.log(cardId);
 
-  // Display card user just flipped in console.
-  console.log("User flipped " + cards[cardId].rank);
-
-  // Add card to array of cards in play
+  // Add rank to array of cards in play
   cardsInPlay.push(cards[cardId].rank);
 
-  // Display image path in console
-  console.log(cards[cardId].cardImage)
-
-  // Display suit in console
-  console.log(cards[cardId].suit)
+  // Display card's image
+  this.setAttribute('src', cards[cardId].cardImage);
 
   // Check if two cards have been played
   if (cardsInPlay.length === 2) {
@@ -59,6 +56,30 @@ var flipCard = function (cardId) {
   }
 };
 
-flipCard(0);
+// Function to create board
+var createBoard = function () {
+  // Loop through cards array to create card elements for board
+  for (var i = 0; i < cards.length; i++) {
+    // Create img element which will be used as a card
+    var cardElement = document.createElement('img');
 
-flipCard(2);
+    // Set src attribute to display back of card image
+    cardElement.setAttribute('src', 'images/back.png');
+
+    // Set card's 'data-id' attribute to be index of current element
+    // data- attributes are meant to store data about an element ot tied to style.
+    cardElement.setAttribute('data-id', i);
+
+    // Add event listener so that when card is clicked, 
+    // function flipCard will be executed
+    cardElement.addEventListener('click', flipCard);
+
+    // Append card to board
+    document.getElementById('game-board').appendChild(cardElement);
+
+  }
+
+}
+
+// Call createBoard function to create board.
+createBoard();
